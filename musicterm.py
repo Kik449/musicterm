757614f8538e8ae0d961444efa4775b3cbe50812
s0ck37 <61667997+Kik449@users.noreply.github.com>
2021-07-21 08:51:40 +0200

    Added configuration file support

--- musicterm.py
+++ musicterm.py
@@ -8,12 +8,13 @@ songs = {}
 pla_l = []
 son_l = []
 alldata = {}
+config = {}
 
 def search(content,limit):
     return_dict = {}
     search = VideosSearch(content, limit = limit)
     count = 0
-    for video in search.result()["result"]:
+    for video in search.result()["result"]:        
         count += 1
         title = video["title"]
         if len(title) > 40:
@@ -21,9 +22,9 @@ def search(content,limit):
             title = title[:-ex]+"..."
         if len(title) < 40:
             title += " "*(40-len(title))
-        print(Fore.LIGHTRED_EX+str(count)+Fore.RESET+": ("+Fore.BLUE+title+Fore.RESET+")---("+Fore.GREEN+video["duration"]+Fore.RESET+")")
+        print(config["theme"]["color1"]+str(count)+Fore.RESET+": ("+config["theme"]["color2"]+title+Fore.RESET+")---("+config["theme"]["color3"]+video["duration"]+Fore.RESET+")")
         return_dict[count-1] = { "title": content.replace(" ","_"),"url":video["link"],"time": video["duration"]}
-    option = input(Fore.YELLOW+"Select a song"+Fore.RESET+"~$ ")
+    option = input(config["theme"]["color4"]+"Select a song"+Fore.RESET+"~$ ")
     return return_dict[int(option)-1]
 
 def base64_encoding(text):
@@ -48,7 +49,7 @@ def add_to_playlist(song,playlist):
     playlists[playlist]["songs"].append(song)
 
 def play_song(title):
-    os.system("ffplay -autoexit -loglevel quiet -volume 20 -nodisp "+title+".mp4 ")
+    os.system("ffplay -autoexit -loglevel quiet -volume " + str(config["volume"])+" -nodisp "+title+".mp4 ")
 
 def create_playlist(name):
     playlists[name] = {}
@@ -60,18 +61,18 @@ def display_playlists():
     count = 0
     for playlist in pla_l:
         count += 1
-        print(Fore.RED+str(count)+Fore.RESET+": ("+Fore.BLUE+playlist["name"]+Fore.RESET+")---("+Fore.GREEN+str(len(playlist["songs"]))+Fore.RESET+")")
+        print(config["theme"]["color1"]+str(count)+Fore.RESET+": ("+config["theme"]["color2"]+playlist["name"]+Fore.RESET+")---("+config["theme"]["color3"]+str(len(playlist["songs"]))+Fore.RESET+")")
 
 def play_playlist(playlist):
     for song in playlists[playlist]["songs"]:
-        print("\b\b"+Fore.LIGHTRED_EX+"Playing: "+Fore.MAGENTA+ song["title"]+Fore.YELLOW+" ^C "+Fore.GREEN+">>"+Fore.RESET)
+        print("\b\b"+config["theme"]["color1"]+"Playing: "+config["theme"]["color5"]+ song["title"]+config["theme"]["color4"]+" ^C "+config["theme"]["color3"]+">>"+Fore.RESET)
         play_song("musicterm/"+base64_encoding(song["title"]))
 
 def display_songs(playlist):
     count = 0
     for song in playlists[playlist]["songs"]:
         count += 1
-        print(Fore.RED+str(count)+Fore.RESET+": ("+Fore.BLUE+song["title"]+Fore.RESET+")---("+Fore.GREEN+song["time"]+Fore.RESET+")")
+        print(config["theme"]["color1"]+str(count)+Fore.RESET+": ("+config["theme"]["color2"]+song["title"]+Fore.RESET+")---("+config["theme"]["color2"]+song["time"]+Fore.RESET+")")
 
 def delete_playlist(playlist):
     del playlists[playlist]
@@ -87,6 +88,7 @@ def delete_song(song):
         count += 1
         if s["title"] == song["title"]:
             del son_l[count-1]
+
 
 def delete_from_playlist(playlist,song):
     count = 0
@@ -110,11 +112,10 @@ def display_all_songs():
     count = 0
     for song in son_l:
         count += 1
-        print(Fore.RED+str(count)+Fore.RESET+": ("+Fore.BLUE+song["title"]+Fore.RESET+")---("+Fore.GREEN+song["time"]+Fore.RESET+")")       
+        print(Fore.LIGHTRED_EX+str(count)+Fore.RESET+": ("+Fore.BLUE+song["title"]+Fore.RESET+")---("+Fore.GREEN+song["time"]+Fore.RESET+")")       
 
-banner = pyfiglet.figlet_format("musicterm", font = "slant"  )
-print(Fore.LIGHTGREEN_EX + banner + Fore.RESET)
 try:
+    config = load_json("config.json")
     alldata = load_json("data.json")
     playlists = alldata["playlists"]
     songs = alldata["songs"]
@@ -123,44 +124,46 @@ try:
     exit = False
 except:
     print("No data file founed!")
+banner = pyfiglet.figlet_format(config["banner_text"], font = "slant"  )
+print(config["theme"]["color3"] + banner + Fore.RESET)
 while(not exit):
     try:
-        y = input(Fore.LIGHTRED_EX+"\b\bTERM"+Fore.RESET+"~$ ")
+        y = input(config["theme"]["color1"]+"\b\bTERM"+Fore.RESET+"~$ ")
         y_s = y.split(" ")
         if y_s[0] == "download":
-            song = search(input(Fore.YELLOW+"Song Name"+Fore.RESET+"~$ "),9)
+            song = search(input(config["theme"]["color4"]+"Song Name"+Fore.RESET+"~$ "),9)
             if song["title"] in songs:
                 raise Exception("")
             download_mp3(song["url"],song["title"])
             songs[song["title"]] = song
             son_l.append(song)
-            print(Fore.BLUE+song["title"]+Fore.RESET+" downloaded!")
+            print(config["theme"]["color2"]+song["title"]+Fore.RESET+" downloaded!")
         if y_s[0] == "mkplaylist":
-            pname = input(Fore.YELLOW+"Playlist Name"+Fore.RESET+"~$ ")
+            pname = input(config["theme"]["color4"]+"Playlist Name"+Fore.RESET+"~$ ")
             create_playlist(pname)
         if y_s[0] == "rmplaylist":
-            pname = input(Fore.YELLOW+"Playlist Name"+Fore.RESET+"~$ ")
+            pname = input(config["theme"]["color4"]+"Playlist Name"+Fore.RESET+"~$ ")
             delete_playlist(pname)
         if y_s[0] == "rmsong":
-            sname = input(Fore.YELLOW+"Song Name"+Fore.RESET+"~$ ")
+            sname = input(config["theme"]["color4"]+"Song Name"+Fore.RESET+"~$ ")
             delete_song(songs[sname])
         if y_s[0] == "append":
-            pname = input(Fore.YELLOW+"Playlist Name"+Fore.RESET+"~$ ")
-            sname = input(Fore.YELLOW+"Song Name"+Fore.RESET+"~$ ")
+            pname = input(config["theme"]["color4"]+"Playlist Name"+Fore.RESET+"~$ ")
+            sname = input(config["theme"]["color4"]+"Song Name"+Fore.RESET+"~$ ")
             add_to_playlist(songs[sname],pname)
         if y_s[0] == "dsongs":
-            pname = input(Fore.YELLOW+"Playlist Name"+Fore.RESET+"~$ ")
+            pname = input(config["theme"]["color4"]+"Playlist Name"+Fore.RESET+"~$ ")
             display_songs(pname)
         if y_s[0] == "pplaylist":
-            pname = input(Fore.YELLOW+"Playlist Name"+Fore.RESET+"~$ ")
+            pname = input(config["theme"]["color4"]+"Playlist Name"+Fore.RESET+"~$ ")
             play_playlist(pname)
         if y_s[0] == "rmfp":
-            pname = input(Fore.YELLOW+"Playlist Name"+Fore.RESET+"~$ ")
-            sname = input(Fore.YELLOW+"Song Name"+Fore.RESET+"~$ ")
+            pname = input(config["theme"]["color4"]+"Playlist Name"+Fore.RESET+"~$ ")
+            sname = input(config["theme"]["color4"]+"Song Name"+Fore.RESET+"~$ ")
             delete_from_playlist(pname,songs[sname])
         if y_s[0] == "psong":
-            sname = input(Fore.YELLOW+"Song Name"+Fore.RESET+"~$ ")
-            print(Fore.LIGHTRED_EX+"\b\bPlaying: "+Fore.MAGENTA+ sname+Fore.YELLOW+" ^C "+Fore.GREEN+">>"+Fore.RESET)
+            sname = input(config["theme"]["color4"]+"Song Name"+Fore.RESET+"~$ ")
+            print(config["theme"]["color1"]+"\b\bPlaying: "+config["theme"]["color5"]+ sname+config["theme"]["color4"]+" ^C "+config["theme"]["color3"]+">>"+Fore.RESET)
             play_song("musicterm/"+base64_encoding(songs[sname]["title"]))
         if y_s[0] == "dplaylists":
             display_playlists()
